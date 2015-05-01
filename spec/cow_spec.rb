b4fb7e2db8a5947fd885d7ca8c331e8553079121
Esther Greatbanks & Juliet Brown <pair+estherlg1.julietbrown84@devbootcamp.com>
2015-05-01 15:45:45 +1200

    tests

--- spec/cow_spec.rb
+++ spec/cow_spec.rb
@@ -3,14 +3,37 @@ require_relative './../app/models/cow'
 require_relative './../app/models/offspring'
 
 describe 'Cow' do
+
   describe '#procreate' do
     it 'returns a random array of animals' do
-      expect(cow.procreate).to be_an_instance_of(Array)
+      expect(Cow.procreate).to be_an_instance_of(Array)
+    end
+  end
+
+  describe '#procreate' do
+    it 'returns an array with a length equal to or greater than 2' do
+      expect(Cow.procreate.length).to be >= (2)
+    end
+  end
+
+  describe '#procreate' do
+    it 'returns an array with a length equal to or less than 4' do
+      expect(Cow.procreate.length).to be <= (4)
+    end
+  end
+
+  describe '#check' do
+    it 'returns a hash' do
+      cow = Cow.new
+      check_return = cow.check
+      expect(check_return).to be_an_instance_of(Hash)
     end
   end
 
   Offspring.destroy_all
 end
+
+
 
 
 
